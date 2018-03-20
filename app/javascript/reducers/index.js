abbbccc18d2a3df45d381de03af1fb24f6d1b087
Wesley Parr <wxp9764@gmail.com>
2018-03-20 14:47:58 -0700

    reducer_index

--- app/javascript/reducers/index.js
+++ app/javascript/reducers/index.js
@@ -1,22 +1,16 @@
 import { combineReducers } from "redux";
-import { DOG_API_CALL_REQUEST } from "../constants/actionTypes";
-import { DOG_API_CALL_SUCCESS } from "../constants/actionTypes";
-import { DOG_API_CALL_FAILURE } from "../constants/actionTypes";
-
-import { CAT_API_CALL_REQUEST } from "../constants/actionTypes";
-import { CAT_API_CALL_SUCCESS } from "../constants/actionTypes";
-import { CAT_API_CALL_FAILURE } from "../constants/actionTypes";
+import * as actionTypes from "../constants/actionTypes";
 
 function dogs(state = { dog: null, error: null, fetching: false }, action) {
   switch (action.type) {
-    case DOG_API_CALL_REQUEST:
+    case actionTypes.DOG_API_CALL_REQUEST:
       return { ...state, fetching: true, error: null };
       break;
-    case DOG_API_CALL_SUCCESS:
+    case actionTypes.DOG_API_CALL_SUCCESS:
       console.log("HELLO DOGS SUCCESS ", action);
       return { ...state, fetching: false, dog: action.dog, cat: null };
       break;
-    case DOG_API_CALL_FAILURE:
+    case actionTypes.DOG_API_CALL_FAILURE:
       return { ...state, fetching: false, dog: null, error: action.error };
       break;
     default:
@@ -26,13 +20,13 @@ function dogs(state = { dog: null, error: null, fetching: false }, action) {
 
 function cats(state = { cat: null, error: null, fetching: false }, action) {
   switch (action.type) {
-    case CAT_API_CALL_REQUEST:
+    case actionTypes.CAT_API_CALL_REQUEST:
       return { ...state, fetching: true, error: null };
       break;
-    case CAT_API_CALL_SUCCESS:
+    case actionTypes.CAT_API_CALL_SUCCESS:
       return { ...state, fetching: false, cat: action.cat, dog: null };
       break;
-    case CAT_API_CALL_FAILURE:
+    case actionTypes.CAT_API_CALL_FAILURE:
       return { ...state, fetching: false, cat: null, error: action.error };
       break;
     default:
